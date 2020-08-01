ce1c4dd9ad766616ae7ec79e494b20b6086bfb7f
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-01 20:37:57 +0200

    code optimized

--- main.ts
+++ main.ts
@@ -1,8 +1,4 @@
 function Page2 () {
-    basic.showString("Hello")
-    basic.showArrow(ArrowNames.South)
-}
-function Page3 () {
     for (let index = 0; index < 4; index++) {
         basic.showIcon(IconNames.Heart)
         basic.pause(100)
@@ -11,43 +7,43 @@ function Page3 () {
     }
     basic.showArrow(ArrowNames.South)
 }
-function Page7 () {
-    basic.showIcon(IconNames.Ghost)
-    basic.showString("Or games")
+function Page3 () {
+    basic.showString("Make a booklet")
+    basic.showArrow(ArrowNames.South)
+}
+function Page1 () {
+    basic.showString("Hello")
     basic.showArrow(ArrowNames.South)
 }
 function Page5 () {
-    basic.showString("6 pages")
-    basic.showArrow(ArrowNames.South)
-}
-function Page6 () {
     basic.showIcon(IconNames.Duck)
     basic.showString("About ducks")
     basic.showArrow(ArrowNames.South)
 }
-function Page4 () {
-    basic.showString("Make a booklet")
+function Page6 () {
+    basic.showIcon(IconNames.Ghost)
+    basic.showString("Or games")
     basic.showArrow(ArrowNames.South)
 }
-let two = 0
-let one = 0
-let zero = 0
+function Page4 () {
+    basic.showString("6 pages")
+    basic.showArrow(ArrowNames.South)
+}
+let pins2 = ""
 basic.showArrow(ArrowNames.South)
 basic.forever(function () {
-    zero = pins.digitalReadPin(DigitalPin.P0)
-    one = pins.digitalReadPin(DigitalPin.P1)
-    two = pins.digitalReadPin(DigitalPin.P2)
-    if (zero == 1 && (one == 0 && two == 0)) {
+    pins2 = "" + pins.digitalReadPin(DigitalPin.P0) + pins.digitalReadPin(DigitalPin.P1) + pins.digitalReadPin(DigitalPin.P2)
+    if (pins2 == "100") {
+        Page1()
+    } else if (pins2 == "010") {
         Page2()
-    } else if (zero == 0 && (one == 1 && two == 0)) {
+    } else if (pins2 == "001") {
         Page3()
-    } else if (zero == 0 && (one == 0 && two == 1)) {
+    } else if (pins2 == "110") {
         Page4()
-    } else if (zero == 1 && (one == 1 && two == 0)) {
+    } else if (pins2 == "101") {
         Page5()
-    } else if (zero == 1 && (one == 0 && two == 1)) {
+    } else if (pins2 == "011") {
         Page6()
-    } else if (zero == 0 && (one == 1 && two == 1)) {
-        Page7()
     }
 })
